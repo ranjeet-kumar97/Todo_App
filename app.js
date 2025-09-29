c51f670085338439ac1d671de999464ceff22407
ranjeet-kumar97 <rk65ranjeet@gmail.com>
2025-09-29 18:41:41 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+let inp=document.querySelector("input");
+let btn=document.querySelector("button");
+let ul=document.querySelector("ul");
+
+btn.addEventListener("click", function(){
+    let item=document.createElement("li");
+    item.innerText=inp.value;
+    ul.appendChild(item);
+
+
+    let delBtn= document.createElement("button");
+    delBtn.innerText="delete";
+    delBtn.classList.add("delete");
+    item.appendChild(delBtn);
+
+
+     console.log(inp.value);
+    inp.value=" ";
+
+   
+   
+});
+
+
+ul.addEventListener("click",function(event){
+    if(event.target.nodeName=="BUTTON"){
+        let listItem= event.target.parentElement;
+        listItem.remove();
+        console.log("deleted");
+    
+    }
+    
+    
+        
+    
+        
+});
+// let delBtns= document.querySelectorAll(".delete");
+// for(delBtn of delBtns){
+
+//     delBtn.addEventListener("click", function(){
+//         let par=this.parentElement;
+//         console.log(par);
+//         par.remove();
+//     });
+// }
+
+
